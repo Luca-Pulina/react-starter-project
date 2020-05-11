29e1584ba2b3a3c224e465ef6981c2b1fdfaf644
lucapulina <lpulina@outlook.it>
2020-05-11 16:18:00 +0200

    scss + normalize.css

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import Home from './components/views/Home'
 
-ReactDOM.render(<Home />, document.getElementById('app'))
+import 'normalize.css/normalize.css'
+import './styles/styles.scss'
+
+ReactDOM.render(<p>This is my boilerplate<p/>, document.getElementById('app'))
+
